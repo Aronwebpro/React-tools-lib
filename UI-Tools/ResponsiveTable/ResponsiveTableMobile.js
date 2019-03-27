229d6d107f181512a9ed4df738fc8d8d0c905494
adeimant <adeimantavicius@plaidypus.com>
2019-03-27 16:21:17 -0500

    Reactive Table

---
+++ UI-Tools/ResponsiveTable/ResponsiveTableMobile.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+
+export default class ResponsiveTableMobile extends React.PureComponent {
+    render() {
+        return (
+            <div>
+            </div>
+        );
+    }
+}
